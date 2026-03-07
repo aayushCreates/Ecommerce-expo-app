75b738f38ea245c533bfdad0e494a041d06d0f5f
AyushAJS <jangidayush89@gmail.com>
2026-03-07 20:08:44 +0530

    frontend init

---
+++ admin/src/main.tsx
@@ -0,0 +1,23 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
+);
