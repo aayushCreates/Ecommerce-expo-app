4376486ad2513416e78b1158bb9b42aa24b6e083
AyushAJS <jangidayush89@gmail.com>
2026-03-07 19:19:55 +0530

    cleark middleware is added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.config";
-import cors from 'cors';
-import morgan from 'morgan';
+import cors from "cors";
+import morgan from "morgan";
 import userRoutes from "./routes/user.routes";
 import reviewRoutes from "./routes/review.routes";
 import orderRoutes from "./routes/order.routes";
@@ -10,25 +10,26 @@ import productRoutes from "./routes/product.routes";
 import cartRoutes from "./routes/cart.routes";
 import paymentRouter from "./routes/payment.routes";
 import paymentRoutes from "./routes/payment.routes";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
-dotenv.config(); 
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(clerkMiddleware()); //  adds auth object under the req => req.auth
 
-
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/reviews', reviewRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/payment', paymentRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/payment", paymentRoutes);
 
 app.get("/api/health", (req, res) => {
-    res.status(200).json({ message: "Success" });
-  });
+  res.status(200).json({ message: "Success" });
+});
 
 const port = process.env.PORT;
 connectDB()
