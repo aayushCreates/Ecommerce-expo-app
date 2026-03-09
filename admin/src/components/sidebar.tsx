0e179995e20f77c67568ce6639a7a2981c069938
AyushAJS <jangidayush89@gmail.com>
2026-03-09 16:27:03 +0530

    IN sidebar ecommerce name updation

--- admin/src/components/sidebar.tsx
+++ admin/src/components/sidebar.tsx
@@ -18,11 +18,11 @@ function Sidebar() {
       <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
         <div className="p-4 w-full">
           <div className="flex items-center gap-3">
-            <div className="size-10 bg-primary rounded-xl flex items-center justify-center shrink-0">
+            <div className="size-10 bg-primary rounded-lg flex items-center justify-center shrink-0">
               <ShoppingBagIcon className="w-6 h-6 text-primary-content" />
             </div>
             <span className="text-xl font-bold is-drawer-close:hidden">
-              Admin
+              Shoppy
             </span>
           </div>
         </div>
@@ -34,7 +34,7 @@ function Sidebar() {
               <li key={item.path}>
                 <Link
                   to={item.path}
-                  className={`is-drawer-close:tooltip is-drawer-close:tooltip-right 
+                  className={`is-drawer-close:tooltip is-drawer-close:tooltip-right rounded-lg 
                     ${isActive ? "bg-primary text-primary-content" : ""}
                   `}
                 >
