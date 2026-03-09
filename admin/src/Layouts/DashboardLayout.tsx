ad06f142c5ec58f46516a34785f4b4d8892caf1e
AyushAJS <jangidayush89@gmail.com>
2026-03-09 13:16:10 +0530

    dashboard layout, navbar and side bar is add along with clerk AUTH

--- admin/src/Layouts/DashboardLayout.tsx
+++ admin/src/Layouts/DashboardLayout.tsx
@@ -1,15 +1,28 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/navbar";
-import Sidebar from "../components/sidebar";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const DashboardLayout = () => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
-      <Outlet />
+      <div className="drawer lg:drawer-open">
+        <input
+          id="my-drawer"
+          type="checkbox"
+          className="drawer-toggle"
+          defaultChecked
+        />
+
+        <div className="drawer-content">
+          <Navbar />
+          <main className="p-6">
+            <Outlet />
+          </main>
+        </div>
+        <Sidebar />
+      </div>
     </div>
-  )
+  );
 };
 
 export default DashboardLayout;
